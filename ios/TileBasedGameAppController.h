65542c05d193b71ae50459daa8369961f29f5ca1
clawoo <alin.claudiu.radut@gmail.com>
2011-08-17 19:23:50 +0300

    Add project files

---
+++ ios/TileBasedGameAppController.h
@@ -0,0 +1,14 @@
+//
+//  TileBasedGameAppController.h
+//  TileBasedGame
+//
+//  Created by Clawoo on 8/17/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+@interface TileBasedGameAppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@end
+
